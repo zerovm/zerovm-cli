6fd1667c7f507a3bb314f1cddf977cd1ce4e1aa5
Martin Geisler <martin@geisler.net>
2014-03-18 12:13:15 +0100

    doc: use sphinx-argparse to include cmdline args
    This makes it easy to include up-to-date command line synopsis in out
    documentation.

--- doc/conf.py
+++ doc/conf.py
@@ -30,6 +30,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinxarg.ext',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
