zerovm/zerovm-cli

commit 8c496e191d985de2e8186fd4c08acb4f9c325f89

bundle: rewrite to use a zar.json file
The bundle command now walks up to the project root automatically. It
then packs all files referenced in the "bundling" section of the
zar.json file. This is a list of glob patterns which are expanded
relative to the project root.

zpmlib/commands.py
@@ -48,9 +48,8 @@ def bundle(parser):
     """Bundle a ZeroVM application"""
 
     def cmd(args):
-        zpm.bundle_project(args.dir)
+        root = zpm.find_project_root()
+        zpm.bundle_project(root)
 
-    parser.add_argument('dir', help='ZeroVM workspace',
-                        metavar='WORKING_DIR', nargs='?',
-                        default=os.getcwd())
+
     parser.set_defaults(func=cmd)

zpmlib/zpm.py
@@ -14,6 +14,8 @@
 
 import os
 import tarfile
+import glob
+import json
 
 from os import path
 
@@ -75,34 +77,35 @@ def _create_project(location):
         fp.write(ZAR_INI_TEMPLATE)
 
 
-def bundle_project(location):
+def find_project_root():
+    root = os.getcwd()
+    while not os.path.isfile(os.path.join(root, 'zar.json')):
+        oldroot, root = root, os.path.dirname(root)
+        if root == oldroot:
+            raise RuntimeError("no zar.json file found")
+    return root
+
+
+def bundle_project(root):
     """
-    Bundle the project given the root project directory as `location`.
+    Bundle the project under root.
     """
-    location = path.abspath(location)
+    zar_json = os.path.join(root, 'zar.json')
+    zar = json.load(open(zar_json))
 
-    zar_ini = path.join(location, 'zar.ini')
-    meta_ini = path.join(location, 'meta.ini')
+    zar_name = zar['meta']['name'] + '.zar'
 
-    # read zar.ini
-    with open(zar_ini) as fp:
-        tars = get_tars_from_zar_ini(fp)
-        tars = list(tars)
-    # TODO(LB): Maybe need a `zpm --bundle --recreate` to force recreation of
-    # cached tars.
+    tar = tarfile.open(zar_name, 'w:gz')
 
-    # now bundle up all of those tars (by tar location specified in the zar.ini
-    # into the .zar,
-    # plus the zar.ini and meta.ini
-    # use the dirname as the zar name
-    zar_name = '%s.zar' % path.split(location)[1]
-    zar = tarfile.open(name=zar_name, mode='w')
-    try:
-        for each_file in tars + [zar_ini, meta_ini]:
-            zar.add(each_file, path.basename(each_file))
-    finally:
-        zar.close()
-    # TODO(LB): Return or print the generated .zar filename?
+    for pattern in zar['bundling'] + ['zar.json']:
+        for path in glob.glob(os.path.join(root, pattern)):
+            print('adding', path)
+            relpath = os.path.relpath(path, root)
+            info = tarfile.TarInfo(name=relpath)
+            info.size = os.path.getsize(path)
+            tar.addfile(info, open(path, 'rb'))
+    tar.close()
+    print('created', zar_name)
 
 
 def make_tar(tar_fp, path, arcpath):

commit cc8bca9ded46696923ade5dc59207c4258446a86

deploy: uncompress zar when uploading
The middleware can only handle user images in the form of uncompressed
tarballs. Trying to mount a tar.gz file resulted in a silent error.

The error was accidentally introduced in 8c496e19 when I rewrote the
code to use zar.json to produce a tarball.

zpmlib/commands.py
@@ -16,6 +16,7 @@ import os
 import json
 import operator
 import tarfile
+import gzip
 import shlex
 import argparse
 try:
@@ -162,7 +163,7 @@ def deploy(args):
     client.auth()
 
     path = '%s/%s' % (args.target, os.path.basename(args.zar))
-    client.upload(path, open(args.zar).read())
+    client.upload(path, gzip.open(args.zar).read())
 
     swift_path = urlparse.urlparse(client._swift_url).path
     if swift_path.startswith('/v1/'):