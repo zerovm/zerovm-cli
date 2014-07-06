c13ce041f5be6194dfeb5025b56f24506a419d91
Constantine Peresypkin <constantine.peresypk@rackspace.com>
2014-07-06 19:18:23 +0300

    added functional tests for zvm-image args
    added more func tests for various `--zvm-image` arguments

--- zvshlib/tests/functional/tests.py
+++ zvshlib/tests/functional/tests.py
@@ -1,9 +1,11 @@
 import os
+import tarfile
 from tempfile import mkstemp, mkdtemp
 import unittest
 import errno
 from shutil import rmtree
 import sys
+from StringIO import StringIO
 import pytest
 import zvshlib
 from zvshlib.zvsh import Shell, DEFAULT_LIMITS
@@ -121,6 +123,20 @@ class TestZvsh(unittest.TestCase):
             ]
         }
         return reference
+
+    def _create_tar(self, name_and_file):
+        tarfd, tarname = mkstemp(dir=self.testdir)
+        os.close(tarfd)
+        tar = tarfile.open(name=tarname, mode='w')
+        for name, f in name_and_file.iteritems():
+            info = tarfile.TarInfo(name)
+            f.seek(0, 2)
+            size = f.tell()
+            info.size = size
+            f.seek(0, 0)
+            tar.addfile(info, f)
+        tar.close()
+        return tarname
 
     def test_one_command(self):
         cmd_line = self.program
@@ -298,6 +314,194 @@ class TestZvsh(unittest.TestCase):
         finally:
             shell.zvsh.orig_cleanup()
 
+    def test_image(self):
+        img1 = self._create_tar({'file1': StringIO('a'),
+                                 'file2': StringIO('b')})
+        img1_dev = '/dev/1.%s' % os.path.basename(img1)
+        img2 = self._create_tar({'file1': StringIO('a'),
+                                 'file2': StringIO('b')})
+        img2_dev = '/dev/2.%s' % os.path.basename(img2)
+        opts = '--zvm-image=%s --zvm-image=%s,/lib' % (img1, img2)
+        self.argv = [ZVSH]
+        self.argv.extend(opts.split())
+        self.argv.append(self.program)
+        cmd_line = self.program
+        shell = Shell(self.argv)
+        try:
+            with pytest.raises(SystemExit):
+                shell.run()
+            files = os.listdir(shell.zvsh.tmpdir)
+            self.assertTrue('manifest.1' in files)
+            self.assertTrue('nvram.1' in files)
+            manifest = _read_manifest(join_path(shell.zvsh.tmpdir,
+                                                'manifest.1'))
+            reference = self._reference_manifest(shell.zvsh.tmpdir)
+            channels = reference['channel']
+            img1_chan = [os.path.abspath(img1),
+                         img1_dev,
+                         '3',
+                         '0',
+                         str(DEFAULT_LIMITS['reads']),
+                         str(DEFAULT_LIMITS['rbytes']),
+                         str(DEFAULT_LIMITS['writes']),
+                         str(DEFAULT_LIMITS['wbytes'])]
+            img2_chan = [os.path.abspath(img2),
+                         img2_dev,
+                         '3',
+                         '0',
+                         str(DEFAULT_LIMITS['reads']),
+                         str(DEFAULT_LIMITS['rbytes']),
+                         str(DEFAULT_LIMITS['writes']),
+                         str(DEFAULT_LIMITS['wbytes'])]
+            # insert in reverse order
+            channels.insert(3, img2_chan)
+            channels.insert(3, img1_chan)
+            self.assertEqual(manifest, reference)
+            nvram = _read_nvram(join_path(shell.zvsh.tmpdir,
+                                          'nvram.1'))
+            reference = _reference_nvram(
+                cmd_line,
+                images=[(img1_dev, '/', 'ro', 'no'),
+                        (img2_dev, '/lib', 'ro', 'no')])
+            self.assertEqual(nvram, reference)
+        finally:
+            shell.zvsh.orig_cleanup()
+
+    def test_image_extract(self):
+        img1 = self._create_tar({'file1': StringIO('a'),
+                                 'file2': StringIO('b')})
+        img1_dev = '/dev/1.%s' % os.path.basename(img1)
+        img2 = self._create_tar({'file3': StringIO('a'),
+                                 'file4': StringIO('b')})
+        img2_dev = '/dev/2.%s' % os.path.basename(img2)
+        opts = '--zvm-image=%s --zvm-image=%s,/lib' % (img1, img2)
+        self.program = 'file2'
+        self.argv = [ZVSH]
+        self.argv.extend(opts.split())
+        self.argv.append(self.program)
+        cmd_line = self.program
+        shell = Shell(self.argv)
+        try:
+            with pytest.raises(SystemExit):
+                shell.run()
+            files = os.listdir(shell.zvsh.tmpdir)
+            self.assertTrue('manifest.1' in files)
+            self.assertTrue('nvram.1' in files)
+            manifest = _read_manifest(join_path(shell.zvsh.tmpdir,
+                                                'manifest.1'))
+            boot = os.path.join(os.path.abspath(shell.zvsh.tmpdir), 'boot.1')
+            reference = self._reference_manifest(shell.zvsh.tmpdir,
+                                                 executable=boot)
+            channels = reference['channel']
+            img1_chan = [os.path.abspath(img1),
+                         img1_dev,
+                         '3',
+                         '0',
+                         str(DEFAULT_LIMITS['reads']),
+                         str(DEFAULT_LIMITS['rbytes']),
+                         str(DEFAULT_LIMITS['writes']),
+                         str(DEFAULT_LIMITS['wbytes'])]
+            img2_chan = [os.path.abspath(img2),
+                         img2_dev,
+                         '3',
+                         '0',
+                         str(DEFAULT_LIMITS['reads']),
+                         str(DEFAULT_LIMITS['rbytes']),
+                         str(DEFAULT_LIMITS['writes']),
+                         str(DEFAULT_LIMITS['wbytes'])]
+            # insert in reverse order
+            channels.insert(3, img2_chan)
+            channels.insert(3, img1_chan)
+            self.assertEqual(manifest, reference)
+            nvram = _read_nvram(join_path(shell.zvsh.tmpdir,
+                                          'nvram.1'))
+            reference = _reference_nvram(
+                cmd_line,
+                images=[(img1_dev, '/', 'ro', 'no'),
+                        (img2_dev, '/lib', 'ro', 'no')])
+            self.assertEqual(nvram, reference)
+        finally:
+            shell.zvsh.orig_cleanup()
+
+    def test_wo_image(self):
+        img1 = self._create_tar({'file1': StringIO('a'),
+                                 'file2': StringIO('b')})
+        img1_dev = '/dev/1.%s' % os.path.basename(img1)
+        opts = '--zvm-image=%s,/,wo' % img1
+        self.argv = [ZVSH]
+        self.argv.extend(opts.split())
+        self.argv.append(self.program)
+        cmd_line = self.program
+        shell = Shell(self.argv)
+        try:
+            with pytest.raises(SystemExit):
+                shell.run()
+            files = os.listdir(shell.zvsh.tmpdir)
+            self.assertTrue('manifest.1' in files)
+            self.assertTrue('nvram.1' in files)
+            manifest = _read_manifest(join_path(shell.zvsh.tmpdir,
+                                                'manifest.1'))
+            reference = self._reference_manifest(shell.zvsh.tmpdir)
+            channels = reference['channel']
+            img1_chan = [os.path.abspath(img1),
+                         img1_dev,
+                         '3',
+                         '0',
+                         str(DEFAULT_LIMITS['reads']),
+                         str(DEFAULT_LIMITS['rbytes']),
+                         str(DEFAULT_LIMITS['writes']),
+                         str(DEFAULT_LIMITS['wbytes'])]
+            channels.insert(3, img1_chan)
+            self.assertEqual(manifest, reference)
+            nvram = _read_nvram(join_path(shell.zvsh.tmpdir,
+                                          'nvram.1'))
+            reference = _reference_nvram(
+                cmd_line,
+                images=[(img1_dev, '/', 'wo', 'no')])
+            self.assertEqual(nvram, reference)
+        finally:
+            shell.zvsh.orig_cleanup()
+
+    def test_ro_wo_image(self):
+        img1 = self._create_tar({'file1': StringIO('a'),
+                                 'file2': StringIO('b')})
+        img1_dev = '/dev/1.%s' % os.path.basename(img1)
+        opts = '--zvm-image=%s,/,ro --zvm-image=%s,/,wo' % (img1, img1)
+        self.argv = [ZVSH]
+        self.argv.extend(opts.split())
+        self.argv.append(self.program)
+        cmd_line = self.program
+        shell = Shell(self.argv)
+        try:
+            with pytest.raises(SystemExit):
+                shell.run()
+            files = os.listdir(shell.zvsh.tmpdir)
+            self.assertTrue('manifest.1' in files)
+            self.assertTrue('nvram.1' in files)
+            manifest = _read_manifest(join_path(shell.zvsh.tmpdir,
+                                                'manifest.1'))
+            reference = self._reference_manifest(shell.zvsh.tmpdir)
+            channels = reference['channel']
+            img1_chan = [os.path.abspath(img1),
+                         img1_dev,
+                         '3',
+                         '0',
+                         str(DEFAULT_LIMITS['reads']),
+                         str(DEFAULT_LIMITS['rbytes']),
+                         str(DEFAULT_LIMITS['writes']),
+                         str(DEFAULT_LIMITS['wbytes'])]
+            channels.insert(3, img1_chan)
+            self.assertEqual(manifest, reference)
+            nvram = _read_nvram(join_path(shell.zvsh.tmpdir,
+                                          'nvram.1'))
+            reference = _reference_nvram(
+                cmd_line,
+                images=[(img1_dev, '/', 'ro', 'no'),
+                        (img1_dev, '/', 'wo', 'no')])
+            self.assertEqual(nvram, reference)
+        finally:
+            shell.zvsh.orig_cleanup()
+
 
 def _read_manifest(file_name):
     with open(file_name) as manifest:
@@ -350,7 +554,7 @@ def _read_nvram(file_name):
         return result
 
 
-def _reference_nvram(command_line, additional_channels=None):
+def _reference_nvram(command_line, additional_channels=None, images=None):
     reference = {
         'args': {
             'args': command_line
@@ -379,4 +583,13 @@ def _reference_nvram(command_line, additional_channels=None):
         reference['mapping'] = reference['mapping'][0]
     elif len(reference['mapping']) == 0:
         del reference['mapping']
+    if images:
+        reference['fstab'] = []
+        for dev, mp, flags, removable in images:
+            reference['fstab'].append({'access': flags,
+                                       'mountpoint': mp,
+                                       'removable': removable,
+                                       'channel': dev})
+        if len(reference['fstab']) == 1:
+            reference['fstab'] = reference['fstab'][0]
     return reference
